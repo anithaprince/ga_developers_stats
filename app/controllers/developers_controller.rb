31142fc87584362a0b2ef10f2ce04f57224a9aa1
Anitha Prince <anithaprince@Princes-MacBook-Pro.local>
2018-11-17 15:42:03 -0500

    created controller file

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,8 +1,27 @@
-class DeveloperController < ApplicationController
+class DevelopersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  # get index (all)
   def index
-        render json: { message: 'hi', status: 200 }
+    render json: Developer.all
+  end
+
+  # get one (by id)
+  def show
+    render json: Developer.find(params["id"])
+  end
+
+  # create just the developer
+  def create
+    render json: Developer.create(params["developer"])
+  end
+
+  def delete
+    render json: Developer.delete(params["id"])
+  end
+
+  def update
+    render json: Developer.update(params["id"], params["developer"])
   end
 
 end
